a3dfc763a4d56006158826f9d667fad6adfc4b15
Sakshi kirola <kirolasakshi@gmail.com>
2026-03-03 20:57:50 +0530

    API key added

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-  
-const MovieCard = ({movie}) => {
- 
-  function favourite(){ 
 
+const MovieCard = ({movie}) => {
+
+  function favourite(){ 
+   
   }
 
   return (
     <div className='movie-card'>
       <div className='movie-poster'>
-        <img src={movie.url} alt={movie.title} /> 
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} /> 
         <div className="movie-overlay">
-            <button className='favorite-btn' onClick={favourite}>🤍</button>  
+            <button className='favorite-btn' onClick={favourite}>🤍</button>     
         </div>
       </div>
  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MovieCard from '../Components/MovieCard'
+import {searchMovies, getPopularMovies} from "../Services/api"
  
 const Home = () => {
-  const [searchQuery,setSearchQuery] = useState(""); 
+  const [searchQuery,setSearchQuery] = useState("");  
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true); 
 
-  const movies = [
-    {id: 1, title: "John Wick", release_date: "2020"},
-    {id: 2, title: "Terminator", release_date: "1999"}, 
-    {id: 3, title: "The Matrix", release_date: "1998"},  
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async() => {
+      try{
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies); 
+      } catch(err){
+        console.log(err)
+        setError("Failed to load movies...")
+      } 
+      finally{
+        setLoading(false);  
+      }; 
+    }
 
-  const handleSearch = (e) => { 
+    loadPopularMovies();  
+  },[]) 
+
+  const handleSearch = (e) => {  
     e.preventDefault()
     alert(searchQuery) 
     setSearchQuery(""); 
